0eb5c416e1fb22572be82ddb3fd9c2645c2b7207
Raegal596 <david.reid596@yahoo.com.au>
2025-02-17 20:06:34 -0700

    Changed the way the scraper was imported and introduced some functionality tests. Introduced environment variables to login to linkedin.

--- check_company_change.py
+++ check_company_change.py
@@ -1,6 +1,19 @@
 # Import Dependencies
+import os
 
-# Import local libraries
-from context import lib
-import lib.linkedin_scraper as linkedin
+from linkedin_scraper import actions
+from linkedin_scraper import Company
+from linkedin_scraper import Person
 
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+# Get username and password from local .env file.
+email = os.getenv("LINKEDIN_EMAIL")
+password = os.getenv("LINKEDIN_PASSWORD")
+
+
+actions.login(driver, email, password)
+
+person = Person('https://www.linkedin.com/in/david-m-reid/', driver = driver)
+company = Company('https://www.linkedin.com/company/monadical/', driver = driver)
